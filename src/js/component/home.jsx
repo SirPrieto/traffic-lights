6236d33aee1c5cc07866008c50f6c56306ce54c9
SirPrieto <hi@abrahamprieto.com>
2022-02-09 02:14:58 +0000

    Initial Push

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [position, setPosition] = useState("top");
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="containermain">
+			<div className="trafficLight ">
+				<div
+					className={
+						position == "top" ? "red light shiny " : "red light"
+					}></div>
+				<div
+					className={
+						position == "middle"
+							? "light yellow  shiny"
+							: "yellow light"
+					}></div>
+				<div
+					className={
+						position == "bottom"
+							? "light green shiny"
+							: "green light"
+					}></div>
+			</div>
+			<button onClick={() => setPosition("top")}>Red</button>
+			<button onClick={() => setPosition("middle")}>Yellow</button>
+			<button onClick={() => setPosition("bottom")}>Green</button>
 		</div>
 	);
 };
